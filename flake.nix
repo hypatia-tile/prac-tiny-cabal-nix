bedeb4c6af90daa1c46e931f3929cf045ab8f7cb
shinokun <hypatia.tile02021@gmail.com>
2026-07-05 14:42:40 +0900

    feat: build Cabal project using Nix

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "Tiny Cabal package built by Nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
+
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+    pkgsFor = system:
+      import nixpkgs {
+        inherit system;
+      };
+  in {
+    packages = forAllSystems (
+      system: let
+        pkgs = pkgsFor system;
+        hpkgs = pkgs.haskellPackages;
+      in {
+        default = hpkgs.callCabal2nix "tiny-cabal-nix" ./. {};
+      }
+    );
+
+    apps = forAllSystems (system: let
+      pkg = self.packages.${system}.default;
+    in {
+      default = {
+        type = "app";
+        program = "${pkg}/bin/tiny-cabal-nix";
+      };
+    });
+
+    devShells = forAllSystems (system: let
+      pkgs = pkgsFor system;
+      hpkgs = pkgs.haskellPackages;
+    in {
+      default = hpkgs.shellFor {
+        packages = p: [
+          self.packages.${system}.default
+        ];
+
+        nativeBuildInputs = [
+          pkgs.cabal-install
+          hpkgs.ghc
+        ];
+      };
+    });
+  };
+}
